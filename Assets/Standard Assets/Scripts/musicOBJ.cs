93ef80810fe1e6d40c19222abe21a265684d54a1
Eleven <21669120+wednesday2024@users.noreply.github.com>
2026-06-02 21:43:10 -0500

    Update musicOBJ.cs

--- Assets/Standard Assets/Scripts/musicOBJ.cs
+++ Assets/Standard Assets/Scripts/musicOBJ.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 
 public class musicOBJ
 {
+#if UNITY_IOS && !UNITY_EDITOR
 	[DllImport("__Internal")]
 	private static extern bool _IsMusicPlaying();
+#endif
 
-	public static bool isMusicPlaying()
-	{
-		if (Application.platform == RuntimePlatform.IPhonePlayer)
-		{
-			return _IsMusicPlaying();
-		}
+    public static bool isMusicPlaying()
+    {
+#if UNITY_IOS && !UNITY_EDITOR
+		return _IsMusicPlaying();
+#elif UNITY_ANDROID && !UNITY_EDITOR
 		return false;
-	}
+#else
+        return false;
+#endif
+    }
 }
